09193f8fe3587276b9223291d06ffa3a5f208ec8
Sebastian Rodriguez <sebastianrodriguezrosas.9@gmail.com>
2025-04-24 11:42:43 -0500

    Se configuro el login

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class);
+
 
 Route::prefix('/products')->controller(ProductController::class)->group(function(){
 
@@ -20,3 +20,8 @@ Route::prefix('/products')->controller(ProductController::class)->group(function
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'welcome'] );
